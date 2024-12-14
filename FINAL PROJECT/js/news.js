f6baf222c88b532576b7456bb6e80596870f9340
SANDRICKSON <sandroqatamadze802@gmail.com>
2024-12-14 19:54:11 +0400

    Update news.js

--- FINAL PROJECT/js/news.js
+++ FINAL PROJECT/js/news.js
@@ -8,6 +8,7 @@ const translations = {
         gallery: "Gallery",
         timetable: "Timetable",
         news: "News",
+        project:"About Project"
         "news-header": "News",
         "news-text": "Chiatura's cable car 'Sashevardno-Zeda Rgani' has reopened.",
         "cable": "🚠 In Chiatura, the rehabilitation process of the passenger cable car on the Zeda Rgani (also known as 'Peace') route has been completed. 🔹 The restored cable car was visited today by Levan Kiknavelidze, the Deputy Governor of Imereti, Paata Kvizinadze, the MP of the Sachkhere-Chiatura-Kharagauli constituency, Givi Modebadze, the Mayor of Chiatura, Aleksandre Tsitskishvili, the Chairman of the City Council, and local government representatives. ✅ The unique public-passenger cableway, constructed under the leadership of engineer-constructor Giorgi Fantsulai in 1966, has been granted the status of a cultural heritage monument and thus retains its authenticity. Only the technical part has been renewed — the gondolas, machinery, and cables. 🔹 The project was funded by the central budget, with a total cost of 4,600,000 GEL. Additionally, the rehabilitation of the Chiatura-Perivisi (the so-called '25') cableway will soon be completed. 🚠 Locals and visitors to Chiatura will benefit from the new passenger cable car transport. ✅ Notably, the opening ceremony of the rehabilitated cable car was attended by Shota Nioradze, a member of Giorgi Fantsulai's team, the former chief energy engineer, and the first passenger."
