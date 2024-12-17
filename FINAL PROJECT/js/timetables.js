5b4d8675d2192645eed6d27f2922b804b0e9bdc0
SANDRICKSON <sandroqatamadze802@gmail.com>
2024-12-17 10:40:49 +0400

    Update timetables.js

--- FINAL PROJECT/js/timetables.js
+++ FINAL PROJECT/js/timetables.js
@@ -1,5 +1,14 @@
 const translations = {
     en: {
+        "bus-schedule":"Bus schedule: Chilovani-Didi Katskhi",
+        "price":"price 1 Lari",
+        "chilovani":"Chilovani",
+        "katskhi":"Didi Katskhi",
+        "bus-timetable":"Bus schedule: Chavchavadze-Memoriali",
+        "price1":"Price: 50 Tetri",
+        "memoriali":"Memoriali",
+        "chavchavadze":"Chavchavadze.str",
+        "cablenaguti":"Cable car directions: Naguti-city, Lezhubani-city, Sanatorium-city, Mukhadze-city Schedule: The car goes down every five minutes from 08:00 to 22:00 Price: 50 tetri",
        "main": "Home",
         "history": "History",
         "culture": "Culture",
@@ -17,6 +26,15 @@ const translations = {
         "culture-text": "The city has cultural institutions such as the Akaki Tsereteli Drama Theatre, a cinema-theatre, the Palace of Rituals, the House of the Artist, the Chiatura Regional Museum, and the Cultural Palace. The cultural education of students in Chiatura is supported by the 'Actors' School' at the Culture Center, the State Ensemble of Dance and Song 'Changi,' folk dance ensembles: 'Merchkhali,' 'Pesevi,' and 'Ensemble-XXI,' the Georgian Folk Song Ensemble 'Imereti,' the School of Pop Music 'Natvrishvalli,' and the Ballroom Dance Studios 'Dance Forum,' 'Gracia,' and 'Elegance.' Every year on September 29, the population celebrates the traditional folk festival 'Chiaturoba,' dedicated to the city of Chiatura."
     },
     ka: {
+        "bus-schedule":"ავტობუსის განრიგი: ჭილოვანი - დიდი კაცხი",
+        "price":"ფასი: 1 ლარი",
+        "chilovani":"ჭილოვანი",
+        "katskhi":"დიდი კაცხი",
+        "bus-timetable":"ავტობუსის განრიგი: მემორიალი - ჭავჭავაძის ქუჩა",
+        "price1":"ფასი: 50 თეთღი",
+        "memoriali":"მემორიალი",
+        "chavchavadze":"ჭავჭავაძის ქუჩა",
+        "cablenaguti":"საბაგიროს მიმართულებები: ნაგუთი-ქალაქი, ლეჟუბანი-ქალაქი, სანატორიუმი-ქალაქი, მუხაძე-ქალაქი. განრიგი:ვაგონი ეშვება ყოველ ხუთ წუთში დილის 08:00-დან საღამოს 22:00-მდე. ფასი: 50 თეთრი",
         "main": "მთავარი",
         "history": "ისტორია",
         "culture": "კულტურა",
